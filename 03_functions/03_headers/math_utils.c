a44c2a4d91a1c6448d9391dfebba5f20d2736d2d
Vahit Mutlu <vahit.mutlu@study.hs-duesseldorf.de>
2026-05-01 09:36:28 +0200

    Day 6.1: header and source file separation, math_utils module

---
+++ 03_functions/03_headers/math_utils.c
@@ -0,0 +1,12 @@
+#include "math_utils.h"
+
+int addition(int a, int b){
+
+    return a + b;
+
+}
+
+int subtraction(int c, int d){
+
+    return c - d;
+}
---
+++ 03_functions/03_headers/math_utils.h
@@ -0,0 +1,8 @@
+# ifndef MATH_UTILS_H
+# define MATH_UTILS_H
+
+int addition(int a, int b);
+
+int subtraction(int c, int d);
+
+#endif
