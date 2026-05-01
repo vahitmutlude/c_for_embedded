71ec46f3e02f8960099e65abde9d37696a9ca36c
Vahit Mutlu <vahit.mutlu@study.hs-duesseldorf.de>
2026-05-01 10:07:26 +0200

    Day 6.2: multi-file compilation, object files, linker

---
+++ 03_functions/04_linker/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(void){
+
+    int addt = addition(45,11);
+    int subt = subtraction(99,10);
+    printf("the addition: %d\n",addt);
+    printf("the subtraction: %d\n",subt);
+    return 0;
+
+}
